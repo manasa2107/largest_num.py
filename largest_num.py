b3ab381ebabc964317cf3c8c6e84d2cf0aa6e500
manasa2107 <82016723+manasa2107@users.noreply.github.com>
2022-01-30 20:09:25 +0530

    Add files via upload

---
+++ largest_num.py
@@ -0,0 +1,8 @@
+from functools import cmp_to_key
+class Solution:
+    def largestNumber(self, A):
+        A = map(str, A)
+	    key = cmp_to_key(lambda a,b: 1 if a+b >= b+a else -1)
+	    res = ''.join(sorted(A, key= key, reverse=True))
+	    res = res.lstrip('0')
+	    return res if res else '0'
